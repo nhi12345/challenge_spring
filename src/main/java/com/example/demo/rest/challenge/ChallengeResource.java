1bc16c7ea3bea9b0e9085aef889926bdb1a6ba0c
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-01 10:32:43 +0700

    refactor code

--- src/main/java/com/example/demo/rest/challenge/ChallengeResource.java
+++ src/main/java/com/example/demo/rest/challenge/ChallengeResource.java
@@ -1,16 +1,9 @@
 package com.example.demo.rest.challenge;
 
-import com.example.demo.domain.Challenge;
-import com.example.demo.integration.database.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @Slf4j
