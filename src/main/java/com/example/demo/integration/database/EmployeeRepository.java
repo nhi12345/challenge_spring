17c492ab17286849eeec3d740eb26efeae0e786e
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-02 10:26:53 +0700

    add function find user by email

--- src/main/java/com/example/demo/integration/database/EmployeeRepository.java
+++ src/main/java/com/example/demo/integration/database/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.integration.database;
 import com.example.demo.domain.employee.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends MongoRepository<Employee,String> {
+    Optional<Employee> findByEmail(String email);
 }
