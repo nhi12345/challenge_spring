27ed0931b4422f4ccc63ce18cbaa7355b5f4f2fa
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-04 13:55:40 +0700

    create submission resource

---
+++ src/main/java/com/example/demo/rest/submission/SubmissionResource.java
@@ -0,0 +1,13 @@
+package com.example.demo.rest.submission;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/publish")
+public class SubmissionResource {
+}
