b064b50362176266b31e706713fda6a55084e41d
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-05 08:43:05 +0700

    map employee to employeedto

---
+++ src/main/java/com/example/demo/rest/employee/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.rest.employee;
+
+import com.example.demo.domain.employee.Employee;
+import com.example.demo.rest.employee.vm.EmployeeDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public abstract class EmployeeMapper {
+    public abstract EmployeeDto toEmployeeDto(Employee employee);
+
+    public List<EmployeeDto> toEmployeeDtos(List<Employee> employees) {
+        return employees.parallelStream().map(this::toEmployeeDto).collect(Collectors.toList());
+    }
+}
