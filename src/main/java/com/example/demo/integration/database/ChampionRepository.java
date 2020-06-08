b685a19d008e82eecb5a111da97fa97c22741e27
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-08 08:32:26 +0700

    add find champion by challenge to repository

--- src/main/java/com/example/demo/integration/database/ChampionRepository.java
+++ src/main/java/com/example/demo/integration/database/ChampionRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.integration.database;
 
+import com.example.demo.domain.challenge.Challenge;
 import com.example.demo.domain.champion.Champion;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ChampionRepository extends MongoRepository<Champion,String> {
+    Optional<Champion> findByChallenge(Challenge challenge);
 }
