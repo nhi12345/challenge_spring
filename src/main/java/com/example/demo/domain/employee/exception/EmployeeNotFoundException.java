791be66b95f6f9fc19a5026092a54200043ec0f1
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-08 17:34:49 +0700

    update employee service

--- src/main/java/com/example/demo/domain/employee/exception/EmployeeNotFoundException.java
+++ src/main/java/com/example/demo/domain/employee/exception/EmployeeNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException{
-    public EmployeeNotFoundException(String employeeId) {
-        super(String.format("Employee with id '%s' not found", employeeId));
+    public EmployeeNotFoundException(String email) {
+        super(String.format("Employee with email '%s' not found", email));
     }
 }
