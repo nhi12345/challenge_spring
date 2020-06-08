c501eeb1c57b8c01b025b75b5279f71624beb9bc
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-08 10:50:01 +0700

    refactor code

--- src/main/java/com/example/demo/domain/champion/ChampionService.java
+++ src/main/java/com/example/demo/domain/champion/ChampionService.java
@@ -2,17 +2,11 @@ package com.example.demo.domain.champion;
 
 import com.example.demo.domain.challenge.Challenge;
 import com.example.demo.domain.challenge.ChallengeService;
-import com.example.demo.domain.employee.Employee;
 import com.example.demo.domain.file.exception.BadRequestException;
 import com.example.demo.domain.file.exception.NotFoundException;
 import com.example.demo.domain.submission.Submission;
 import com.example.demo.domain.submission.SubmissionService;
 import com.example.demo.integration.database.ChampionRepository;
-import com.example.demo.integration.database.EmployeeRepository;
-import com.example.demo.integration.database.SubmissionRepository;
-import com.example.demo.rest.champion.ChampionMapper;
-import com.example.demo.rest.champion.vm.ChampionResponse;
-import com.example.demo.rest.submission.SubmissionMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,18 +28,6 @@ public class ChampionService {
     @Autowired
     private SubmissionService submissionService;
 
-    @Autowired
-    private ChampionMapper championMapper;
-
-    @Autowired
-    private SubmissionMapper submissionMapper;
-
-    @Autowired
-    private SubmissionRepository submissionRepository;
-
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
     public Champion addChampion (Champion champion, String submissionId){
         Challenge currentChallenge = challengeService.getCurrentChallenge();
         Optional<Champion> championOptional =championRepository.findByChallenge(Challenge.builder().id(currentChallenge.getId()).build());
@@ -64,17 +46,12 @@ public class ChampionService {
         return championRepository.save(champion);
     }
 
-    public ChampionResponse getChampion(){
+    public Champion getChampion(){
         Challenge currentChallenge = challengeService.getCurrentChallenge();
         Optional<Champion> champion =championRepository.findByChallenge(Challenge.builder().id(currentChallenge.getId()).build());
         if(!champion.isPresent()){
             throw new BadRequestException("Champion not found!");
         }
-        ChampionResponse championResponse = new ChampionResponse();
-        Submission submission = submissionRepository.findById(champion.get().getSubmission().getId()).get();
-        championResponse.setPerson(submissionMapper.mapPeople(employeeRepository.findByEmail(submission.getEmployee().getEmail()).get()));
-        championResponse.setChampion(championMapper.toChampionDto(champion.get()));
-        championResponse.setVideo(submissionMapper.toSubmissionDto(submission));
-        return championResponse;
+        return champion.get();
     }
 }
