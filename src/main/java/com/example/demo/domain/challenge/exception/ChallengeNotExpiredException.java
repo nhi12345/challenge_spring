019024d5f0e7890067d9d2316cb8524dabb14b2a
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-09 09:56:40 +0700

    change location of ChallengeNotExpiredException

--- src/main/java/com/example/demo/domain/challenge/exception/ChallengeNotExpiredException.java
+++ src/main/java/com/example/demo/domain/challenge/exception/ChallengeNotExpiredException.java
@@ -1,4 +1,4 @@
-package com.example.demo.domain.champion.exception;
+package com.example.demo.domain.challenge.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- src/main/java/com/example/demo/domain/champion/ChampionService.java
+++ src/main/java/com/example/demo/domain/champion/ChampionService.java
@@ -2,7 +2,7 @@ package com.example.demo.domain.champion;
 
 import com.example.demo.domain.challenge.Challenge;
 import com.example.demo.domain.challenge.ChallengeService;
-import com.example.demo.domain.champion.exception.ChallengeNotExpiredException;
+import com.example.demo.domain.challenge.exception.ChallengeNotExpiredException;
 import com.example.demo.domain.champion.exception.ChampionAlreadyExistsException;
 import com.example.demo.domain.champion.exception.ChampionNotFoundException;
 import com.example.demo.domain.submission.Submission;
