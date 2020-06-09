6381653ced9e457c6c5a3f0ad47b6ccf886c0891
Nhi Nguyen <nhinguyen@novahub.vn>
2020-06-09 09:59:21 +0700

    check challenge expired

--- src/main/java/com/example/demo/domain/challenge/ChallengeService.java
+++ src/main/java/com/example/demo/domain/challenge/ChallengeService.java
@@ -1,10 +1,13 @@
 package com.example.demo.domain.challenge;
 
+import com.example.demo.domain.challenge.exception.ChallengeNotExpiredException;
 import com.example.demo.integration.database.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,13 @@ public class ChallengeService {
 
     public Challenge saveChallenge(Challenge challenge){
         return challengeRepository.save(challenge);
+    }
+
+    public boolean challengeIsExpired(Challenge challenge){
+        if (LocalDate.now().isBefore(challenge.getStartDate()) || LocalDate.now().isAfter(challenge.getEndDate())) {
+            throw new ChallengeNotExpiredException();
+        }
+        return false;
     }
 
 }
